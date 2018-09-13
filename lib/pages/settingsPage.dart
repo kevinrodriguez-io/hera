d3ce47ed9eea81608ad444ba1c9f6e94c9aa759c
Kevin Rodríguez <_@kevinrodriguez.io>
2018-09-12 23:45:02 -0600

    Adding settings page

--- lib/pages/settingsPage.dart
+++ lib/pages/settingsPage.dart
@@ -12,6 +12,39 @@ class _SettingsPageState extends State<SettingsPage> {
       appBar: AppBar(
         title: Text('Settings'),
       ),
+      body: Container(
+        padding: EdgeInsets.all(20.0),
+        child: (
+          ListView(
+            children: <Widget>[
+              FlutterLogo(
+                style: FlutterLogoStyle.stacked,
+                size: 100.0,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 20.0),
+                child: Text(
+                  "Hera app built with love by kevinrodriguez-io \nThis app uses the concept of 'pages', some of you might be familiar if you are coming from a Xamarin / IONIC background. In this case we have routes, themes, navigation, login and register. Firebase integration coming soon.",
+                  style: TextStyle(color: Color.fromARGB(255, 200, 200, 200)),
+                ),
+              ),
+              FlatButton(
+                textColor: Theme.of(context).accentColor,
+                onPressed: (){},
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text('Log Out', style: TextStyle(color: Colors.redAccent),),
+                    Spacer(),
+                    Icon(Icons.exit_to_app, color: Colors.redAccent,)
+                  ],
+                )
+                //Text('Log out'),
+              )
+            ],
+          )
+        ),
+      ),
     );
   }
 }
